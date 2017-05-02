305e1b4225e3ddaa3bf4a542e64e79f2c3f408c5
jdurrant <jacobdurrant@gmail.com>
2017-05-02 15:30:58 -0400

    Renamed bash files to cmd.

--- webapp/compile.cmd
+++ webapp/compile.cmd
@@ -1,1 +1,10 @@
-tsc --target ES5 --module amd main.ts proteinvr/RequireConfig.ts proteinvr/Environment.ts proteinvr/CameraChar.ts --allowUnreachableCode --pretty
+#!/bin/bash
+#cd ../
+
+
+tsc --target ES5 --module amd main.ts proteinvr/Settings/UserVars.ts proteinvr/Core/Setup.ts proteinvr/RequireConfig.ts proteinvr/Environment.ts proteinvr/CameraChar.ts
+
+#find proteinvr -name "*.js" -exec rm '{}' \;
+#find proteinvr -name "*.ts" | awk '{print "tsc --target ES5 --module amd " $1}' | parallel -j6
+
+echo Done
--- webapp/main.js
+++ webapp/main.js
@@ -1,9 +1,12 @@
 // THIS IS WHERE ANY CUSTOM CODE GOES. DEFINING SHADERS AND TRIGGERS AND SUCH.
-define(["require", "exports", "./proteinvr/Events/Event", "./proteinvr/Events/Actions/AdministerQuiz", "./proteinvr/Events/TriggerConditionals/KeyPress", "./proteinvr/Events/TriggerConditionals/ClickedObject", "./proteinvr/Events/Actions/LabelOnMesh"], function (require, exports, Event_1, AdministerQuiz_1, KeyPress_1, ClickedObject_1, LabelOnMesh_1) {
+define(["require", "exports", "./proteinvr/Events/Event", "./proteinvr/Events/TriggerConditionals/DistanceToMesh", "./proteinvr/Events/Actions/FadeOutMesh"], function (require, exports, Event_1, DistanceToMesh_1, FadeOutMesh_1) {
     "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var jQuery = PVRGlobals.jQuery;
+    // declare var Core: any;  // attached to window in RequireConfig.ts
     // this function was added in so the app could be run from the config.ts file with requirejs
     // jQuery is passed as an arg so this function and any module it utilizes can use jquery from the config.ts path object
-    function start() {
+    function start(Core) {
         var setEvents = function () {
             /**
             A function to register any events.
@@ -11,89 +14,95 @@ define(["require", "exports", "./proteinvr/Events/Event", "./proteinvr/Events/Ac
             // new Event(
             //     new GameStart({}, jQuery),
             //     new MoveCamera({
-            //         camera: CameraChar.camera,
+            //         camera: PVRGlobals.camera,
             //         milliseconds: 1000,
-            //         startPoint: CameraChar.camera.position,
-            //         endPoint: new BABYLON.Vector3(CameraChar.camera.position.x + 25, CameraChar.camera.position.y, CameraChar.camera.position.z)
+            //         startPoint: PVRGlobals.camera.position,
+            //         endPoint: new BABYLON.Vector3(PVRGlobals.camera.position.x + 25, PVRGlobals.camera.position.y, PVRGlobals.camera.position.z)
             //     })
             // );
-            new Event_1.default(new KeyPress_1.default({
-                event: 'keypress',
-                action: new AdministerQuiz_1.default({
-                    quiz: {
-                        "name": "Sample Quiz",
-                        "questions": {
-                            "question1": {
-                                "question": "What is my favorite color?",
-                                "option1": "Green",
-                                "option2": "Blue",
-                                "option3": "Red",
-                                "option4": "Orange",
-                                "answer": "Blue"
-                            },
-                            "question2": {
-                                "question": "Where am I?",
-                                "option1": "Here",
-                                "option2": "There",
-                                "option3": "Everywhere",
-                                "option4": "Nowhere",
-                                "answer": "Here"
-                            }
-                        }
-                    },
-                    length: 2
-                })
-            }));
+            // new Event(
+            //     new KeyPress({
+            //         event: 'keypress',
+            //         action: new AdministerQuiz({
+            //             quiz: {
+            //                     "name": "Sample Quiz",
+            //                     "questions" : {
+            //                        "question1": {
+            //                             "question": "What is my favorite color?",
+            //                             "option1": "Green",
+            //                             "option2": "Blue",
+            //                             "option3": "Red",
+            //                             "option4": "Orange",
+            //                             "answer": "Blue"
+            //                         },
+            //                     "question2": {
+            //                             "question": "Where am I?",
+            //                             "option1": "Here",
+            //                             "option2": "There",
+            //                             "option3": "Everywhere",
+            //                             "option4": "Nowhere",
+            //                             "answer": "Here"
+            //                         }
+            //                     }  
+            //                 },
+            //             length: 2
+            //         })
+            //     })
+            // );
             // new Event(
             //     new DistanceToMesh({
-            //         triggerMesh: Core.meshesByName["surf_trgt"], 
+            //         triggerMesh: PVRGlobals.meshesByName["surf_trgt"], 
             //         cutOffDistance: 9.0
             //     }),
             //     new ScreenWhite({
-            //         mesh: Core.meshesByName["surf"], 
+            //         mesh: PVRGlobals.meshesByName["surf"], 
             //         milliseconds: 2000
             //     })   
             // );
-            // new Event(
-            //     new DistanceToMesh({
-            //         triggerMesh: Core.meshesByName["surf_trgt"], 
-            //         cutOffDistance: 9.0
-            //     }),
-            //     new MoveCamera({
-            //         camera: CameraChar.camera,
-            //         milliseconds: 1000,
-            //         startPoint: CameraChar.camera.position,
-            //         endPoint: new BABYLON.Vector3(CameraChar.camera.position.x + 25, CameraChar.camera.position.y, CameraChar.camera.position.z)
-            //     })
-            // );
+            new Event_1.default(new DistanceToMesh_1.default({
+                triggerMesh: PVRGlobals.meshesByName["NA_patch_single_outer"],
+                cutOffDistance: 2.0
+            }), new FadeOutMesh_1.default({
+                mesh: PVRGlobals.meshesByName["NA_patch_single_outer"],
+                milliseconds: 2000
+            })
+            // new MoveCamera({
+            //     camera: PVRGlobals.camera,
+            //     milliseconds: 1000,
+            //     startPoint: PVRGlobals.camera.position,
+            //     endPoint: new BABYLON.Vector3(PVRGlobals.camera.position.x + 25, PVRGlobals.camera.position.y, PVRGlobals.camera.position.z)
+            // })
+            );
             // new Event(
             // new GameStart({}, jQuery),
             // new MoveCamera({
-            // camera: CameraChar.camera,
+            // camera: PVRGlobals.camera,
             // milliseconds: 1000,
-            // startPoint: CameraChar.camera.position,
-            // endPoint: new BABYLON.Vector3(CameraChar.camera.position.x + 25, CameraChar.camera.position.y, CameraChar.camera.position.z)
+            // startPoint: PVRGlobals.camera.position,
+            // endPoint: new BABYLON.Vector3(PVRGlobals.camera.position.x + 25, PVRGlobals.camera.position.y, PVRGlobals.camera.position.z)
             // }),
             // true,
             // jQuery
             // );
             // new Event(
             //     new ClickedObject({
-            //         triggerMesh: Core.meshesByName["grnd"],
+            //         triggerMesh: PVRGlobals.meshesByName["grnd"],
             //         action: new MoveCamera({
-            //             camera: CameraChar.camera,
+            //             camera: PVRGlobals.camera,
             //             milliseconds: 1000,
-            //             startPoint: CameraChar.camera.position,
-            //             endPoint: new BABYLON.Vector3(CameraChar.camera.position.x + 25, CameraChar.camera.position.y, CameraChar.camera.position.z)
+            //             startPoint: PVRGlobals.camera.position,
+            //             endPoint: new BABYLON.Vector3(PVRGlobals.camera.position.x + 25, PVRGlobals.camera.position.y, PVRGlobals.camera.position.z)
             //         })
             //     }, Core),           
             // );
-            new Event_1.default(new ClickedObject_1.default({
-                triggerMesh: Core.meshesByName["grnd"],
-                action: new LabelOnMesh_1.default({
-                    label: "Hi :)",
-                })
-            }, Core));
+            // new Event(
+            //     new ClickedObject({
+            //      triggerMesh: PVRGlobals.meshesByName["grnd"],
+            //         action: new LabelOnMesh({
+            //             label: "Hi :)",
+            //         })
+            //     }, Core),
+            // );
         };
         return setEvents;
     }
