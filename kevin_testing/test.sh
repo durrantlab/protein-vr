9aaacce756725857b173dc8cff57260887e37f70
Jacob Durrant <jdurrant@MacBook-Pro.fios-router.home>
2019-04-21 01:41:11 -0400

    Updates.

--- kevin_testing/test.sh
+++ kevin_testing/test.sh
@@ -1,5 +1,11 @@
 echo "Git pull to make sure you have the latest code..."
 git pull
 
-echo "Rsync the latest code to the web directory..."
+echo "Copy the latest code to the web directory..."
 cp ../biotite_app/build/* ./web/
+
+echo "Change into the web directory..."
+cd web
+
+echo "Start a python server..."
+python ../server.py
